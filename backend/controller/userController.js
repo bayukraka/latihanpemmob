8e103fb700478540ce02149302a9e64281b65bcd
bayukraka <bayu.kraka@gmail.com>
2022-07-21 09:43:10 +0700

    register user ok

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -13,6 +13,37 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw Error('Please add all fields')
     }
+
+    // check if user exist
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw Error('User already exist')
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw Error('Invalid user data')
+    }
+
     res.json({ message: "Register user"})
 })
 
