b5e54dc914e80dcd3b1d187ed693e3b47e6d9624
bayukraka <bayu.kraka@gmail.com>
2022-07-21 12:05:49 +0700

    role in get all goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,12 +1,21 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler (async (req, res)=>{
-    const goals = await Goal.find({ user: req.user.id })
-    res.status(200).json(goals)
+    const users = await User.findById(req.user.id)
+
+    if ( users.role == "admin") {
+        const goals = await Goal.find()
+        res.status(200).json(goals)
+    } else {
+        const goals = await Goal.find({ user: req.user.id })
+        res.status(200).json(goals)
+    }
+    
 })
 
 // @desc    Set goals
