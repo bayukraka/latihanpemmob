22141fab7f91293574a074f2c89af253cc040af4
bayukraka <bayu.kraka@gmail.com>
2022-07-21 11:28:04 +0700

    add role in userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,6 +14,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: [true, 'Please add a password']
     },
+    role: {
+        type: String,
+        required: [true, 'Please add a role']
+    },
 },
 {
     timestamps: true
